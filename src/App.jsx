530f7d7eb1eb5c93928b019b9dc46994d8065ab2
Bharathi07 <bk120498@gmail.com>
2024-01-01 15:39:56 +0530

    sdfds

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter,Route,Routes} from "react-router-dom";
 
 function App() {
   return <>
-  <BrowserRouter basename="/ems-frontend">
+  <BrowserRouter>
     <HeaderComponent/>
     <Routes>
         {/* // http://localhost:3000 */}
